19e14f8b5ca33d194c37e77d87233f6b48b3a9f9
decanus <dean.eigenmann@icloud.com>
2019-07-03 18:59:13 -0400

    rough client

---
+++ client/Client.go
@@ -0,0 +1,37 @@
+/// This package contains the implementation of a `dasy` client.
+package client
+
+/// The dasy client.
+type Client struct {
+
+}
+
+/// Invite invites a peer to a chat.
+func (c *Client) Invite()  {
+
+}
+
+/// Join joins a chat.
+func (c *Client) Join()  {
+
+}
+
+/// Leave leaves a chat.
+func (c *Client) Leave()  {
+
+}
+
+/// Kick kicks peer from a chat.
+func (c *Client) Kick()  {
+
+}
+
+/// Ack acknowledges `Join`, `Leave` and `Kick` messages.
+func (c *Client) Ack()  {
+
+}
+
+/// Post sends a message to a chat.
+func (c *Client) Post()  {
+
+}
