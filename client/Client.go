31c01c18caf616a6075e2084c7ca765065d8b138
decanus <dean.eigenmann@icloud.com>
2019-07-03 19:13:44 -0400

    types

--- client/Client.go
+++ client/Client.go
@@ -1,37 +1,44 @@
 /// This package contains the implementation of a `dasy` client.
 package client
 
+import (
+	mvds "github.com/status-im/mvds/node"
+	"github.com/status-im/mvds/state"
+)
+
+type Chat state.GroupID
+
 /// The dasy client.
 type Client struct {
-
+	node mvds.Node
 }
 
 /// Invite invites a peer to a chat.
-func (c *Client) Invite()  {
+func (c *Client) Invite(chat Chat, peer []byte)  {
 
 }
 
 /// Join joins a chat.
-func (c *Client) Join()  {
+func (c *Client) Join(chat Chat)  {
 
 }
 
 /// Leave leaves a chat.
-func (c *Client) Leave()  {
+func (c *Client) Leave(chat Chat)  {
 
 }
 
 /// Kick kicks peer from a chat.
-func (c *Client) Kick()  {
+func (c *Client) Kick(chat Chat, peer []byte)  {
 
 }
 
 /// Ack acknowledges `Join`, `Leave` and `Kick` messages.
-func (c *Client) Ack()  {
+func (c *Client) Ack(chat Chat, messageID []byte)  {
 
 }
 
 /// Post sends a message to a chat.
-func (c *Client) Post()  {
+func (c *Client) Post(chat Chat, body []byte)  {
 
 }
