9873b5a2746fec3a84f2854de316267c40a87ebc
decanus <dean.eigenmann@icloud.com>
2019-07-04 10:55:19 -0400

    fmt

--- client/Client.go
+++ client/Client.go
@@ -15,31 +15,31 @@ type Client struct {
 }
 
 // Invite invites a peer to a chat.
-func (c *Client) Invite(chat Chat, peer []byte)  {
+func (c *Client) Invite(chat Chat, peer []byte) {
 
 }
 
 // Join joins a chat.
-func (c *Client) Join(chat Chat)  {
+func (c *Client) Join(chat Chat) {
 
 }
 
 // Leave leaves a chat.
-func (c *Client) Leave(chat Chat)  {
+func (c *Client) Leave(chat Chat) {
 
 }
 
 // Kick kicks peer from a chat.
-func (c *Client) Kick(chat Chat, peer []byte)  {
+func (c *Client) Kick(chat Chat, peer []byte) {
 
 }
 
 // Ack acknowledges `Join`, `Leave` and `Kick` messages.
-func (c *Client) Ack(chat Chat, messageID []byte)  {
+func (c *Client) Ack(chat Chat, messageID []byte) {
 
 }
 
 // Post sends a message to a chat.
-func (c *Client) Post(chat Chat, body []byte)  {
+func (c *Client) Post(chat Chat, body []byte) {
 
 }
