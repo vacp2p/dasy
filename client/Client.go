c9a069e580fc589c30042093c2e94e41acdd1163
decanus <dean.eigenmann@icloud.com>
2019-07-04 10:42:04 -0400

    fix

--- client/Client.go
+++ client/Client.go
@@ -1,4 +1,4 @@
-/// This package contains the implementation of a `dasy` client.
+// This package contains the implementation of a `dasy` client.
 package client
 
 import (
@@ -8,37 +8,37 @@ import (
 
 type Chat state.GroupID
 
-/// The dasy client.
+// The dasy client.
 type Client struct {
 	node mvds.Node
 }
 
-/// Invite invites a peer to a chat.
+// Invite invites a peer to a chat.
 func (c *Client) Invite(chat Chat, peer []byte)  {
 
 }
 
-/// Join joins a chat.
+// Join joins a chat.
 func (c *Client) Join(chat Chat)  {
 
 }
 
-/// Leave leaves a chat.
+// Leave leaves a chat.
 func (c *Client) Leave(chat Chat)  {
 
 }
 
-/// Kick kicks peer from a chat.
+// Kick kicks peer from a chat.
 func (c *Client) Kick(chat Chat, peer []byte)  {
 
 }
 
-/// Ack acknowledges `Join`, `Leave` and `Kick` messages.
+// Ack acknowledges `Join`, `Leave` and `Kick` messages.
 func (c *Client) Ack(chat Chat, messageID []byte)  {
 
 }
 
-/// Post sends a message to a chat.
+// Post sends a message to a chat.
 func (c *Client) Post(chat Chat, body []byte)  {
 
 }
